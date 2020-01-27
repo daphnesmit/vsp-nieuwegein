ec61ba61ae0a810335b8b1f62459b1a67f11dc17
Daphne Smit <daphnesmit1986@gmail.com>
2020-01-27 19:02:11 +0100

    Set up pages on netlifycms

--- src/pages/index.md
+++ src/pages/index.md
@@ -2,3 +2,4 @@
 templateKey: index-page
 title: Great coffee with a conscience
 image: /img/home-jumbotron.jpg
+---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,19 +7,28 @@ import Layout from "../components/Layout"
 export const HomePageTemplate = ({ title, image }) => {
   console.log(image)
   return (
-    <h1
-      className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+    <div
       style={{
-        boxShadow:
-          "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-        backgroundColor: "rgb(255, 68, 0)",
-        color: "white",
-        lineHeight: "1",
-        padding: "0.25em",
+        backgroundImage: `url(${
+          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+        })`,
+        backgroundPosition: `top left`,
+        backgroundAttachment: `fixed`,
       }}
     >
-      {title}
-    </h1>
+      <h1
+        className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+        style={{
+          boxShadow:
+            "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+          color: "white",
+          height: "100vh",
+          padding: "0.25em",
+        }}
+      >
+        {title}
+      </h1>
+    </div>
   )
 }
 HomePageTemplate.propTypes = {
@@ -49,10 +58,16 @@ export default HomePage
 
 export const pageQuery = graphql`
   query HomePageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "home-page" } }) {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
